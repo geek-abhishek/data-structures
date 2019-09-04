4a7a6b8361f2cbf33e901f7f2edbb856f91da730
abhishekdas <abhdas@microsoft.com>
2019-09-04 13:01:55 +0530

    Longest Palindrome using Dynamic Programming

---
+++ Algorithms/src/StringAlgorithms/DynamicProgramming/LongestPalindrome.cs
@@ -0,0 +1,60 @@
+﻿namespace StringAlgorithms
+{
+    public class LongestPalindrome
+    {
+        public string GetLongestPalindrome(string s)
+        {
+            if (string.IsNullOrWhiteSpace(s))
+            {
+                return s;
+            }
+
+            var palindromeTable = new bool[s.Length, s.Length];
+
+            // Since a string with only 1 character is always a palindrome
+            var maxLength = 1;
+            for (int i = 0; i < s.Length; i++)
+            {
+                palindromeTable[i, i] = true;
+            }
+
+            // Check for substrings of length 2
+            var start = 0;
+            for (int i = 0; i < s.Length - 1; i++)
+            {
+                if (s[i].Equals(s[i + 1]))
+                {
+                    palindromeTable[i, i + 1] = true;
+                    start = i;
+                    maxLength = 2;
+                }
+            }
+
+            // Check for substrings of length > 2.
+            for (int k = 3; k <= s.Length; k++)
+            {
+                for (int i = 0; i < s.Length - k + 1; i++)
+                {
+                    // Get the ending index of substring
+                    // from starting index 'i' and length 'k'
+                    var j = i + k - 1;
+
+                    // Checking for substring from 'i'th index to 'j'th index
+                    // iff s[i+1] to s[j-1] is a palindrome 
+                    if (palindromeTable[i + 1, j - 1] && s[i].Equals(s[j]))
+                    {
+                        palindromeTable[i, j] = true;
+
+                        if (k > maxLength)
+                        {
+                            start = i;
+                            maxLength = k;
+                        }
+                    }
+                }
+            }
+
+            return s.Substring(start, maxLength);
+        }
+    }
+}
---
+++ Algorithms/test/StringAlgorithms.Test/LongestPalindromeTests.cs
@@ -0,0 +1,30 @@
+﻿namespace StringAlgorithms.Test
+{
+    using Xunit;
+    using FluentAssertions;
+
+    public class LongestPalindromeTests
+    {
+        [Theory]
+        [InlineData("", "")]
+        [InlineData("z", "z")]
+        [InlineData("bb", "bb")]
+        [InlineData("eabcb", "bcb")]
+        [InlineData("bbabbad", "bbabb")]
+        [InlineData("forgeeksskeegfor", "geeksskeeg")]
+        [InlineData("civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth", "ranynar")]
+        public void GetLongestPalindrome_ShouldReturn_TheLongestPalindromeInTheString(
+            string input,
+            string expectedOutput)
+        {
+            // Arrange
+            var longestPalindrome = new LongestPalindrome();
+
+            // Act
+            var output = longestPalindrome.GetLongestPalindrome(input);
+
+            // Assert
+            output.Should().Be(expectedOutput);
+        }
+    }
+}
