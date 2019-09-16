4b023120b9191031a69855719d0e3a7d59378e4b
abhishekdas <abhdas@microsoft.com>
2019-09-16 19:00:34 +0530

    MaximumSubarray

---
+++ Algorithms/src/DynamicProgramming/MaximumSubarray.cs
@@ -0,0 +1,48 @@
+﻿namespace DynamicProgramming
+{
+    public class MaximumSubarray
+    {
+        public int GetMaximumSubarray(int[] numbers)
+        {
+            var maximumCumulativeSum = numbers[0];
+            var cumulativeSum = 0;
+
+            for (var i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] >= 0)
+                {
+                    maximumCumulativeSum = 0;
+                    cumulativeSum = 0;
+                    break;
+                }
+
+                if (maximumCumulativeSum < numbers[i])
+                {
+                    maximumCumulativeSum = numbers[i];
+                }
+            }
+
+            if (maximumCumulativeSum < 0)
+            {
+                return maximumCumulativeSum;
+            }
+
+            for (var i = 0; i < numbers.Length; i++)
+            {
+                cumulativeSum = cumulativeSum + numbers[i];
+
+                if (cumulativeSum < 0)
+                {
+                    cumulativeSum = 0;
+                }
+
+                if (maximumCumulativeSum < cumulativeSum)
+                {
+                    maximumCumulativeSum = cumulativeSum;
+                }
+            }
+
+            return maximumCumulativeSum;
+        }
+    }
+}
---
+++ Algorithms/test/DynamicProgramming.Test/MaximumSubarrayTests.cs
@@ -0,0 +1,26 @@
+﻿namespace DynamicProgramming.Test
+{
+    using FluentAssertions;
+    using Xunit;
+
+    public class MaximumSubarrayTests
+    {
+        [Theory]
+        [InlineData(new int[] { -1 }, -1)]
+        [InlineData(new int[] { -1, -2 }, -1)]
+        [InlineData(new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 }, 6)]
+        [InlineData(new int[] { -2, -3, 4, -1, -2, 1, 5, -3 }, 7)]
+        public void GetMaximumSubarray_ShouldReturn_MaximumSubarrayInArray(
+            int[] numbers, int expectedSumOfMaximumSubarray)
+        {
+            // Arrange
+            var maximumSubArray = new MaximumSubarray();
+
+            // Act
+            var sumOfMaximumSubarray = maximumSubArray.GetMaximumSubarray(numbers);
+
+            // Assert
+            sumOfMaximumSubarray.Should().Be(expectedSumOfMaximumSubarray);
+        }
+    }
+}
