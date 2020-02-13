3b80f912098bd24a0cfe730b39f47e11f2428764
abhishekdas <abhdas@microsoft.com>
2020-02-13 16:02:45 +0530

    Solved StockTrader

--- Algorithms/src/DynamicProgramming/StockTrader.cs
+++ Algorithms/src/DynamicProgramming/StockTrader.cs
@@ -1,25 +1,144 @@
 ﻿namespace DynamicProgramming
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
 
     public class StockTrader
     {
-        public int GetMaximumProfit(int[] prices)
+        public int GetMaximumProfitForSingleTransaction(int[] prices)
         {
-            var maximumProfitInNegative = 0;
+            if (prices.Any() == false)
+            {
+                return 0;
+            }
+
+            if (prices.Length.Equals(1))
+            {
+                return 0;
+            }
+
+            var maximumProfit = 0;
 
             for (var i = 0; i < prices.Length; i++)
             {
                 for (var j = i + 1; j < prices.Length; j++)
                 {
-                    if (prices[i] - prices[j] < maximumProfitInNegative)
+                    if (prices[j] - prices[i] > maximumProfit)
                     {
-                        maximumProfitInNegative = prices[i] - prices[j];
+                        maximumProfit = prices[j] - prices[i];
                     }
                 }
             }
 
-            return Math.Abs(maximumProfitInNegative);
+            return maximumProfit;
+        }
+
+        public int GetMaximumProfitForMultipleTransactions(int[] prices)
+        {
+            if (prices.Any() == false)
+            {
+                return 0;
+            }
+
+            prices = GetReducedPrices(prices);
+
+            if (prices.Length.Equals(1))
+            {
+                return 0;
+            }
+
+            var tabulatedPrices = new int[prices.Length, prices.Length];
+
+            for (var i = 0; i < prices.Length; i++)
+            {
+                for (var j = i + 1; j < prices.Length; j++)
+                {
+                    tabulatedPrices[i, j] = prices[j] - prices[i] <= 0 ? 0 : prices[j] - prices[i];
+                }
+            }
+
+            for (var i = prices.Length - 2; i >= 0; i--)
+            {
+                var previousMaximumProfit = 0;
+
+                for (var j = prices.Length - 1; j > i; j--)
+                {
+                    var maximumProfit = 0;
+
+                    if (previousMaximumProfit.Equals(0) == false)
+                    {
+                        maximumProfit = tabulatedPrices[j, j + 1] > previousMaximumProfit
+                            ? tabulatedPrices[j, j + 1]
+                            : previousMaximumProfit;
+
+                        tabulatedPrices[i, j] += maximumProfit;
+                        previousMaximumProfit = maximumProfit;
+                        continue;
+                    }
+
+                    for (var k = j + 1; k < prices.Length; k++)
+                    {
+                        if (tabulatedPrices[j, k] > maximumProfit)
+                        {
+                            maximumProfit = tabulatedPrices[j, k];
+                        }
+                    }
+
+                    tabulatedPrices[i, j] += maximumProfit;
+                    previousMaximumProfit = maximumProfit;
+                }
+            }
+
+            var maximumRealizedProfit = tabulatedPrices[0, 1];
+            for (var i = 2; i < prices.Length; i++)
+            {
+                if (tabulatedPrices[0, i] > maximumRealizedProfit)
+                {
+                    maximumRealizedProfit = tabulatedPrices[0, i];
+                }
+            }
+
+            return maximumRealizedProfit;
+        }
+
+        private int[] GetReducedPrices(int[] prices)
+        {
+            var reducedPrices = new List<int>();
+            var isDescending = true;
+
+            for (var i = 0; i < prices.Length - 1; i++)
+            {
+                if (prices[i] < prices[i + 1])
+                {
+                    isDescending = false;
+                }
+
+                if (isDescending == false)
+                {
+                    reducedPrices.Add(prices[i]);
+                }
+            }
+
+            reducedPrices.Add(prices[prices.Length - 1]);
+            var lastNonZeroIndex = reducedPrices.Count - 1;
+
+            for (int i = reducedPrices.Count - 1; i >= 0; i--)
+            {
+                if (reducedPrices[i].Equals(0) == false)
+                {
+                    lastNonZeroIndex = i;
+                    break;
+                }
+            }
+
+            var trimmedPrices = new List<int>();
+            for (var i = 0; i <= lastNonZeroIndex; i++)
+            {
+                trimmedPrices.Add(reducedPrices[i]);
+            }
+
+            return trimmedPrices.ToArray();
         }
     }
 }
--- Algorithms/test/DynamicProgramming.Test/StockTraderTests.cs
+++ Algorithms/test/DynamicProgramming.Test/StockTraderTests.cs
@@ -6,15 +6,34 @@
     public class StockTraderTests
     {
         [Theory]
+        [InlineData(new int[] { 1 }, 0)]
         [InlineData(new int[] { 8, 6, 4, 2, 1 }, 0)]
+        [InlineData(new int[] { 1, 2, 3, 4, 5 }, 4)]
         [InlineData(new int[] { 7, 1, 5, 3, 6, 4 }, 5)]
-        public void GetMaximumProfit_ShouldReturn_MaximumProfit_ForTradedStocks(int[] prices, int expectedProfit)
+        public void GetMaximumProfitForSingleTransaction_ShouldReturn_MaximumProfit_ForTradedStocks(int[] prices, int expectedProfit)
         {
             // Arrange
             var stockTrader = new StockTrader();
 
             // Act
-            var profit = stockTrader.GetMaximumProfit(prices);
+            var profit = stockTrader.GetMaximumProfitForSingleTransaction(prices);
+
+            // Assert
+            profit.Should().Be(expectedProfit);
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1 }, 0)]
+        [InlineData(new int[] { 8, 6, 4, 2, 1 }, 0)]
+        [InlineData(new int[] { 1, 2, 3, 4, 5 }, 4)]
+        [InlineData(new int[] { 7, 1, 5, 3, 6, 4 }, 7)]
+        public void GetMaximumProfitForMultipleTransactions_ShouldReturn_MaximumProfit_ForTradedStocks(int[] prices, int expectedProfit)
+        {
+            // Arrange
+            var stockTrader = new StockTrader();
+
+            // Act
+            var profit = stockTrader.GetMaximumProfitForMultipleTransactions(prices);
 
             // Assert
             profit.Should().Be(expectedProfit);
