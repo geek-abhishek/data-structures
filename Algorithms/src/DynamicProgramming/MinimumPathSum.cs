7a3e44386ec28f54258344e617d8f3840f70e2b0
abhishekdas <abhdas@microsoft.com>
2020-02-27 16:37:31 +0530

    Solved MinimumPathSum

---
+++ Algorithms/src/DynamicProgramming/MinimumPathSum.cs
@@ -0,0 +1,53 @@
+﻿namespace DynamicProgramming
+{
+    public class MinimumPathSum
+    {
+        public int GetMinimumSum(int[][] grid)
+        {
+            var pathGrid = new int[grid.Length][];
+            pathGrid[0] = new int[grid[0].Length];
+            pathGrid[0][0] = grid[0][0];
+
+            for (var i = 1; i < grid.Length; i++)
+            {
+                pathGrid[i] = new int[grid[i].Length];
+                pathGrid[i][0] = grid[i][0] + GetElement(pathGrid, i - 1, 0);
+            }
+
+            for (var i = 1; i < grid[0].Length; i++)
+            {
+                pathGrid[0][i] = grid[0][i] + GetElement(pathGrid, 0, i - 1);
+            }
+
+            for (int i = 1; i < grid.Length; i++)
+            {
+                for (int j = 1; j < grid[i].Length; j++)
+                {
+                    pathGrid[i][j] = GetMinimum(
+                        grid[i][j] + GetElement(pathGrid, i, j - 1),
+                        grid[i][j] + GetElement(pathGrid, i - 1, j));
+                }
+            }
+
+            return pathGrid[grid.Length - 1][pathGrid[grid.Length - 1].Length - 1];
+        }
+
+        private int GetElement(int[][] pathGrid, int row, int col)
+        {
+            if (row < 0)
+            {
+                return 0;
+            }
+
+            if (col < 0)
+            {
+                return 0;
+            }
+
+            return pathGrid[row][col];
+        }
+
+        private int GetMinimum(int element1, int element2) =>
+            element1 < element2 ? element1 : element2;
+    }
+}
---
+++ Algorithms/test/DynamicProgramming.Test/MinimumPathSumTests.cs
@@ -0,0 +1,23 @@
+﻿namespace DynamicProgramming.Test
+{
+    using FluentAssertions;
+    using Xunit;
+
+    public class MinimumPathSumTests
+    {
+        [Theory]
+        [InlineData(6, new int[] { 1, 2, 5 }, new int[] { 3, 2, 1 })]
+        [InlineData(7, new int[] { 1, 3, 1 }, new int[] { 1, 5, 1 }, new int[] { 4, 2, 1 })]
+        public void GetMinimumSum_ShouldReturn_MinimumPathSum(int expectedSum, params int[][] grid)
+        {
+            // Arrange
+            var minimumPath = new MinimumPathSum();
+
+            // Act
+            var minimumPathSum = minimumPath.GetMinimumSum(grid);
+
+            // Assert
+            minimumPathSum.Should().Be(expectedSum);
+        }
+    }
+}
