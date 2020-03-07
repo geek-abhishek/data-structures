9235096d2d2f8c9422e2be86f7eb57081842de5e
abhishekdas <abhdas@microsoft.com>
2020-03-07 12:09:10 +0530

    Implemented HouseRobber

---
+++ Algorithms/src/DynamicProgramming/HouseRobber.cs
@@ -0,0 +1,39 @@
+﻿namespace DynamicProgramming
+{
+    using System;
+    using System.Linq;
+
+    public class HouseRobber
+    {
+        public int Rob(int[] nums)
+        {
+            if (nums == null || nums.Any() == false)
+            {
+                return 0;
+            }
+
+            if (nums.Length.Equals(1))
+            {
+                return nums[0];
+            }
+
+            if (nums.Length.Equals(2))
+            {
+                return Math.Max(nums[0], nums[1]);
+            }
+
+            var maxSums = new int[3];
+            maxSums[0] = nums[0];
+            maxSums[1] = Math.Max(nums[0], nums[1]);
+
+            for (var i = 2; i < nums.Length; i++)
+            {
+                maxSums[2] = Math.Max(nums[i] + maxSums[0], maxSums[1]);
+                maxSums[0] = maxSums[1];
+                maxSums[1] = maxSums[2];
+            }
+
+            return maxSums[2];
+        }
+    }
+}
---
+++ Algorithms/test/DynamicProgramming.Test/HouseRobberTests.cs
@@ -0,0 +1,26 @@
+﻿namespace DynamicProgramming.Test
+{
+    using FluentAssertions;
+    using Xunit;
+
+    public class HouseRobberTests
+    {
+        [Theory]
+        [InlineData(new int[] { 2, 1, 1, 2 }, 4)]
+        [InlineData(new int[] {1, 2, 3, 1}, 4)]
+        [InlineData(new int[] { 2, 7, 9, 3, 1 }, 12)]
+        [InlineData(new int[] { 8, 2, 8, 9, 2 }, 18)]
+        [InlineData(new int[] { 183, 219, 57, 193, 94, 233, 202, 154, 65, 240, 97, 234, 100, 249, 186, 66, 90, 238, 168, 128, 177, 235, 50, 81, 185, 165, 217, 207, 88, 80, 112, 78, 135, 62, 228, 247, 211 }, 3365)]
+        public void Rob_ShouldReturn_MaximumAmount_ThatCanBeRobbed(int[] nums, int expectedAmount)
+        {
+            // Arrange
+            var houseRobber = new HouseRobber();
+
+            // Act
+            var amount = houseRobber.Rob(nums);
+
+            // Assert
+            amount.Should().Be(expectedAmount);
+        }
+    }
+}
