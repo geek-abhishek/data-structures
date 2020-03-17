759d9eca2972feb321f58177e859aa8d97b231fd
abhishekdas <abhdas@microsoft.com>
2020-03-17 23:24:18 +0530

    Implemented MedianCalculator

---
+++ Algorithms/src/DataStructures/MedianCalculator.cs
@@ -0,0 +1,128 @@
+﻿namespace DataStructures
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class MedianCalculator
+    {
+        public double GetMedianOfSortedArrays(int[] nums1, int[] nums2)
+        {
+            int low1 = 0;
+            int high1 = nums1.Length - 1;
+
+            int low2 = 0;
+            int high2 = nums2.Length - 1;
+
+            List<int> numbers = new List<int>();
+
+            if (nums1.Any() == false && nums2.Any() == false)
+            {
+                return 0;
+            }
+
+            if (nums1.Any() == false)
+            {
+                numbers.AddRange(nums2);
+            }
+            else if (nums2.Any() == false)
+            {
+                numbers.AddRange(nums1);
+            }
+            else if (nums1[low1] >= nums2[high2])
+            {
+                numbers.AddRange(nums2);
+                numbers.AddRange(nums1);
+            }
+            else if (nums2[low2] >= nums1[high1])
+            {
+                numbers.AddRange(nums1);
+                numbers.AddRange(nums2);
+            }
+            else
+            {
+                int[] small;
+
+                if (nums1.Length < nums2.Length)
+                {
+                    numbers.AddRange(nums2);
+                    small = nums1;
+                }
+                else
+                {
+                    numbers.AddRange(nums1);
+                    small = nums2;
+                }
+
+                foreach (int num in small)
+                {
+                    int index = GetIndex(numbers, num);
+                    Insert(numbers, num, index);
+                }
+            }
+
+            int medianIndex = numbers.Count / 2;
+
+            if (numbers.Count % 2 != 0)
+            {
+                return (double)numbers[medianIndex];
+            }
+
+            return (double)(numbers[medianIndex] + numbers[medianIndex - 1]) / 2;
+        }
+
+        private int GetIndex(List<int> nums, int target)
+        {
+            int high = nums.Count - 1;
+            int low = 0;
+
+            if (target > nums[high])
+            {
+                return high + 1;
+            }
+
+            if (target < nums[low])
+            {
+                return 0;
+            }
+
+            int lastUpdated = 0;
+
+
+            while (high >= low)
+            {
+                int mid = (low + high) / 2;
+
+                if (target == nums[mid])
+                {
+                    return mid;
+                }
+
+                if (target < nums[mid])
+                {
+                    high = mid - 1;
+                    continue;
+                }
+
+                if (target > nums[mid])
+                {
+                    low = mid + 1;
+                    lastUpdated = low;
+                }
+            }
+
+            return lastUpdated;
+        }
+
+        private void Insert(List<int> numbers, int newElement, int position)
+        {
+            numbers.Add(newElement);
+
+            for (int i = numbers.Count - 1; i > position; i--)
+            {
+                int temp = numbers[i];
+                numbers[i] = numbers[i - 1];
+                numbers[i - 1] = temp;
+            }
+        }
+    }
+}
---
+++ Algorithms/test/DataStructures.Test/MedianCalculatorTests.cs
@@ -0,0 +1,28 @@
+﻿namespace DataStructures.Test
+{
+    using FluentAssertions;
+    using Xunit;
+
+    public class MedianCalculatorTests
+    {
+        [Theory]
+        [InlineData(new int[] { }, new int[] { 1 }, 1)]
+        [InlineData(new int[] { 1, 2 }, new int[] { 3, 4 }, 2.5)]
+        [InlineData(new int[] { 1, 3 }, new int[] { 2 }, 2)]
+        [InlineData(new int[] { 1, 3, 4 }, new int[] { 2 }, 2.5)]
+        public void GetMedianOfSortedArrays_ShouldReturn_MedianOfTwoSortedArrays(
+            int[] nums1,
+            int[] nums2,
+            double expectedMedian)
+        {
+            // Arrange
+            var medianCalculator = new MedianCalculator();
+
+            // Act
+            var median = medianCalculator.GetMedianOfSortedArrays(nums1, nums2);
+
+            // Assert
+            median.Should().Be(expectedMedian);
+        }
+    }
+}
