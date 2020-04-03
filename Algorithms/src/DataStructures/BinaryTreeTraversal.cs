4bb222a6de02c7698aab3807dfb139583e0b0725
abhishekdas <abhdas@microsoft.com>
2020-04-03 15:56:44 +0530

    Implemented Binary Tree Traversal

---
+++ Algorithms/src/DataStructures/BinaryTreeTraversal.cs
@@ -0,0 +1,110 @@
+﻿namespace DataStructures
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class BinaryTree
+    {
+        public int Data { get; set; }
+
+        public BinaryTree Left { get; set; }
+
+        public BinaryTree Right { get; set; }
+
+        public int Level { get; set; }
+
+        public BinaryTree(int data)
+        {
+            this.Data = data;
+            this.Left = this.Right = null;
+            this.Level = 0;
+        }
+
+        public void AddLeft(BinaryTree subtree)
+        {
+            this.Left = subtree;
+            this.Left.Level = this.Level + 1;
+        }
+
+        public void AddRight(BinaryTree subtree)
+        {
+            this.Right = subtree;
+            this.Right.Level = this.Level + 1;
+        }
+    }
+
+    public class BinaryTreeTraversal
+    {
+        public List<int> DepthTraversal(BinaryTree tree, List<int> traversed = null)
+        {
+            if (traversed == null)
+            {
+                traversed = new List<int>();
+            }
+
+            traversed.Add(tree.Data);
+
+            if (tree.Left == null && tree.Right == null)
+            {
+                return traversed;
+            }
+
+            if (tree.Left != null)
+            {
+                this.DepthTraversal(tree.Left, traversed);
+            }
+
+            if (tree.Right != null)
+            {
+                this.DepthTraversal(tree.Right, traversed);
+            }
+
+            return traversed;
+        }
+
+        public List<int> BreadthTraversal(BinaryTree tree)
+        {
+            var height = this.GetHeight(tree);
+            var traversed = new List<int>();
+
+            for (int i = 0; i < height; i++)
+            {
+                this.BreadthTraversal(tree, i, traversed);
+            }
+
+            return traversed;
+        }
+
+        private int GetHeight(BinaryTree tree)
+        {
+            if (tree == null)
+            {
+                return 0;
+            }
+
+            return Math.Max(
+                this.GetHeight(tree.Left),
+                this.GetHeight(tree.Right)) + 1;
+        }
+
+        private List<int> BreadthTraversal(BinaryTree tree, int level, List<int> traversed)
+        {
+            if (tree == null)
+            {
+                return traversed;
+            }
+
+            if (level == 0)
+            {
+                traversed.Add(tree.Data);
+            }
+            else if (level > 0)
+            {
+                this.BreadthTraversal(tree.Left, level - 1, traversed);
+                this.BreadthTraversal(tree.Right, level - 1, traversed);
+            }
+
+            return traversed;
+        }
+    }
+}
---
+++ Algorithms/test/DataStructures.Test/BinaryTreeTraversalTests.cs
@@ -0,0 +1,65 @@
+﻿namespace DataStructures.Test
+{
+    using FluentAssertions;
+    using System.Collections.Generic;
+    using Xunit;
+
+    public class BinaryTreeTraversalTests
+    {
+        [Fact]
+        public void DepthTraversal_ShouldReturn_TraversedNodes_InDepthOrder()
+        {
+            // Arrange
+            var root = new BinaryTree(1);
+            root.AddLeft(new BinaryTree(2));
+            root.AddRight(new BinaryTree(3));
+
+            root.Left.AddLeft(new BinaryTree(4));
+            root.Left.AddRight(new BinaryTree(5));
+
+            root.Right.AddLeft(new BinaryTree(6));
+            root.Right.AddRight(new BinaryTree(7));
+
+            root.Left.Left.AddLeft(new BinaryTree(8));
+            root.Left.Left.AddRight(new BinaryTree(9));
+
+            var expectedTraversed = new List<int> { 1, 2, 4, 8, 9, 5, 3, 6, 7 };
+
+            var treeTraversal = new BinaryTreeTraversal();
+
+            // Act
+            var traversed = treeTraversal.DepthTraversal(root);
+
+            // Assert
+            traversed.Should().BeEquivalentTo(expectedTraversed);
+        }
+
+        [Fact]
+        public void BreadthTraversal_ShouldReturn_TraversedNodes_InLevelOrder()
+        {
+            // Arrange
+            var root = new BinaryTree(1);
+            root.AddLeft(new BinaryTree(2));
+            root.AddRight(new BinaryTree(3));
+
+            root.Left.AddLeft(new BinaryTree(4));
+            root.Left.AddRight(new BinaryTree(5));
+
+            root.Right.AddLeft(new BinaryTree(6));
+            root.Right.AddRight(new BinaryTree(7));
+
+            root.Left.Left.AddLeft(new BinaryTree(8));
+            root.Left.Left.AddRight(new BinaryTree(9));
+
+            var expectedTraversed = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            var treeTraversal = new BinaryTreeTraversal();
+
+            // Act
+            var traversed = treeTraversal.BreadthTraversal(root);
+
+            // Assert
+            traversed.Should().BeEquivalentTo(expectedTraversed);
+        }
+    }
+}
