bca5816c4b4300b889bc9bbe3171605d7a75823b
Abhishek Das <abhdas@microsoft.com>
2020-12-07 18:36:17 +0530

    Dice roll with target sum

---
+++ Algorithms/src/DynamicProgramming/DiceRollWithTargetSum.cs
@@ -0,0 +1,40 @@
+﻿namespace DynamicProgramming
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class DiceRollWithTargetSum
+    {
+        private readonly int modulus = (int)Math.Pow(10, 9) + 7;
+        private readonly Dictionary<string, int> results = new Dictionary<string, int>();
+
+        public int NumRollsToTarget(int d, int f, int target)
+        {
+            if (d == 0 && target == 0)
+            {
+                return 1;
+            }
+
+            if (d == 0)
+            {
+                return 0;
+            }
+
+            var key = $"{target}|{d}";
+            if (results.ContainsKey(key))
+            {
+                return results[key];
+            }
+
+            int numberOfWays = 0;
+            for (int i = 1; i <= f; i++)
+            {
+                numberOfWays =
+                    (numberOfWays + this.NumRollsToTarget(d - 1, f, target - i)) % modulus;
+            }
+
+            results[key] = numberOfWays;
+            return numberOfWays;
+        }
+    }
+}
---
+++ Algorithms/test/DynamicProgramming.Test/DiceRollWithTargetSumTests.cs
@@ -0,0 +1,33 @@
+﻿namespace DynamicProgramming.Test
+{
+    using FluentAssertions;
+    using Xunit;
+
+    public class DiceRollWithTargetSumTests
+    {
+        [Theory]
+        [InlineData(1, 2, 3, 0)]
+        [InlineData(2, 6, 7, 6)]
+        [InlineData(3, 6, 8, 21)]
+        [InlineData(2, 5, 10, 1)]
+        [InlineData(30, 30, 500, 222616187)]
+        public void NumRollsToTarget_ShouldReturn_NumberOfWaysInWhichSumCanBeAchieved(
+            int numberOfDices,
+            int numberofFaces,
+            int sumToAchieve,
+            int expectedNumberOfWays)
+        {
+            // Arrange
+            var diceRoll = new DiceRollWithTargetSum();
+
+            // Act
+            var numberOfWays = diceRoll.NumRollsToTarget(
+                numberOfDices,
+                numberofFaces,
+                sumToAchieve);
+
+            // Assert
+            numberOfWays.Should().Be(expectedNumberOfWays);
+        }
+    }
+}
