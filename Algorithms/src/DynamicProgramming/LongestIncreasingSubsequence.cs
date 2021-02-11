43b5ec6e5f17c4cff2b92a4f7560553348b184e9
Abhishek Das <abhdas@microsoft.com>
2021-02-11 21:34:42 +0530

    Longest Increasing Subsequence

---
+++ Algorithms/src/DynamicProgramming/LongestIncreasingSubsequence.cs
@@ -0,0 +1,54 @@
+﻿namespace DynamicProgramming
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class LongestIncreasingSubsequence
+    {
+        public int LengthOfLIS(int[] nums)
+        {
+            if (nums.Length == 1)
+            {
+                return 1;
+            }
+
+            if (nums.Length == 2)
+            {
+                return nums[0] < nums[1] ? 2 : 1;
+            }
+
+            var lengths = new List<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                lengths.Add(1);
+            }
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    if (j == i)
+                    {
+                        break;
+                    }
+
+                    if (nums[j] < nums[i])
+                    {
+                        lengths[i] = Math.Max(lengths[i], lengths[j] + 1);
+                    }
+                }
+            }
+
+            var maxLength = lengths[0];
+            for(int i = 1; i < lengths.Count; i++)
+            {
+                if (lengths[i] > maxLength)
+                {
+                    maxLength = lengths[i];
+                }
+            }
+
+            return maxLength;
+        }
+    }
+}
---
+++ Algorithms/test/DynamicProgramming.Test/LongestIncreasingSubsequenceTests.cs
@@ -0,0 +1,26 @@
+﻿namespace DynamicProgramming.Test
+{
+    using FluentAssertions;
+    using Xunit;
+
+    public class LongestIncreasingSubsequenceTests
+    {
+        [Theory]
+        [InlineData(new int[] { 10, 9, 2, 5, 3, 7, 101, 18 }, 4)]
+        [InlineData(new int[] { 3, 3, 3, 3 }, 1)]
+        [InlineData(new int[] { 0, 1, 0, 3, 2, 3 }, 4)]
+        public void LengthOfLIS_ShouldReturn_LengthOfLongestIncreasingSubsequence(
+            int[] nums,
+            int expectedResult)
+        {
+            // Arrange
+            var increasingSubsequence = new LongestIncreasingSubsequence();
+
+            // Act
+            var result = increasingSubsequence.LengthOfLIS(nums);
+
+            // Assert
+            result.Should().Be(expectedResult);
+        }
+    }
+}
