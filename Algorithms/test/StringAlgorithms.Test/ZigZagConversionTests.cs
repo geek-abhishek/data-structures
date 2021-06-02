a4affb72571aeec5abb43dcc7d7ee7eb4c8b5c48
Abhishek Das <cse.abhishek.das@gmail.com>
2021-06-03 00:32:08 +0530

    Implemented ZigZagConversion

---
+++ Algorithms/test/StringAlgorithms.Test/ZigZagConversionTests.cs
@@ -0,0 +1,27 @@
+﻿namespace StringAlgorithms.Test
+{
+    using Xunit;
+    using FluentAssertions;
+
+    public class ZigZagConversionTests
+    {
+        [Theory]
+        [InlineData("AB", 1, "AB")]
+        [InlineData("PAYPALISHIRING", 3, "PAHNAPLSIIGYIR")]
+        [InlineData("PAYPALISHIRING", 4, "PINALSIGYAHRPI")]
+        public void Convert_ShouldConvert_StringToZigZag(
+            string input,
+            int numberOfRows,
+            string expectedOutput)
+        {
+            // Arrange
+            var converter = new ZigZagConversion();
+
+            // Act
+            var output = converter.Convert(input, numberOfRows);
+
+            // Assert
+            output.Should().Be(expectedOutput);
+        }
+    }
+}
